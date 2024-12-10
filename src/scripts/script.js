e46a16e447a7debffdea136435450ea3873565fa
Gpeder <gtzyy008@gmail.com>
2024-12-10 11:04:31 -0300

    first commit

---
+++ src/scripts/script.js
@@ -0,0 +1,7 @@
+const close = document.querySelector('.close');
+
+close.addEventListener('click', () => {
+    const modal = document.querySelector('.modal');
+    modal.style.display = 'none';
+})  
+
